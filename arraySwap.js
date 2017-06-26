c7f63584859e38c9255fc87b106cbc918d1d3a1d
loyti <brice@loyti.com>
2017-06-26 11:34:56 -0700

    updated js file

--- arraySwap.js
+++ arraySwap.js
@@ -1,13 +1,12 @@
-var playerFavs = ["Ted", "Pedro", "Wakefield", "Yastrzemski", "Bogaerts", 8, 9, 2]
-var countUp = playerFavs.length;
+var playerFavs = ["Ted", "Pedro", "Wakefield", "Yastrzemski", "Bogaerts", 8, 9, 2];
 var playerNums = [];
 function getPlayerNumbers () {
     document.write()
-    for (var i = 0; i < countUp; i++){
+    for (var i = 0; i < playerFavs.length; i++){
         if (typeof(playerFavs[i] === "number")){
             playerNums.push(i);
-            playerFavs.pop(i);
-            document.write("<p class=\"number\">This is number: " + playerNums[i] + "</p>")
+            document.write("<p class=\"number\">This is number: " + playerFavs[i] + "</p>")
+            document.write("<p> This is the number" + playerNum[i] + "</p>");
         } else { 
             document.write("<p class=\"number\">Meet " + playerFavs[i] + ", he is one of my personal favorites.</p>")
         }
